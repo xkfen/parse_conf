e4a984fe7bf0a7ba758ee748970aab8a16ad6215
xkfen <2547951466@qq.com>
2019-06-03 11:10:45 +0800

    Update main.go
    update import pkgs

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"parse_conf/cache"
-	cfg "parse_conf/config"	
-  "fmt"
+	"github.com/parse_conf/cache"
+	cfg "github.com/parse_conf/config"	
+ 	 "fmt"
 )
 
 
 func main() {
 	// 加载配置信息
 	instance := cfg.GetAppInstance()
-  info = fmt.Sprintf("#####  configuration:\n"+
+  info := fmt.Sprintf("#####  configuration:\n"+
 		"[redis] 		    server   			: %s\n"+
 		"[redis] 		    password 			: %s\n"+
 		"[redis] 		    db       			: %d\n",
-		conf.GetRedisHosts(), conf.GetRedisPassword(),
-		conf.GetRedisDb())
+	  instance.GetRedisHosts(), instance.GetRedisPassword(),
+	  instance.GetRedisDb())
 	fmt.Println(info)
 	cache.CacheDataSource().DataSourceInit(instance.GetRedisHosts(),
 		instance.GetRedisPassword(), instance.GetRedisDb())
